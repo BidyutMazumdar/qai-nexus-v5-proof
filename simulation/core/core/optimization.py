04864c3d1d88333200052aae198b0b5931178119
BidyutMazumdar <bumbamazumdar2017@gmail.com>
2026-03-26 18:41:37 +0530

    Delete simulation directory

--- simulation/core/core/optimization.py
+++
@@ -1,4 +0,0 @@
-def optimize(nodes):
-    for n in nodes:
-        n.C *= (n.E / max(n.L, 0.01))
-    return nodes
--- simulation/core/engine.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, energy, compute, latency):
-        self.E = energy
-        self.C = compute
-        self.L = latency
-
-def sync(nodes):
-    return sum(n.E * n.C * n.L for n in nodes)
